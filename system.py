3ed07c8c2ab5495bd1be9d3351e5306923b6379c
Karthik Gajjala <kgajjala@gmail.com>
2011-04-12 02:44:33 -0700

    changes

--- system.py
+++ system.py
@@ -1,17 +1,48 @@
 import subprocess
 
-UN_RELEASE = subprocess.getoutput("uname -r")
-UN_TZ = subprocess.getoutput("cat /etc/timezone")
-UN_MODEL = subprocess.getoutput("cat /etc/issue")
-UN_SERIAL = subprocess.getoutput("sudo /usr/sbin/dmidecode | grep UUID")
-UN_CPUCOUNTS = subprocess.getoutput("cat /proc/cpuinfo | grep processor | wc -l")
-UN_PHYCPUS = subprocess.getoutput("grep \"physical id\" /proc/cpuinfo | sort -u | wc -l")
-PER_CORES = subprocess.getoutput("grep \"cores\" /proc/cpuinfo | sort -u | cut -d\':\' -f2")
+def exec_command(cmdList):
+  p = subprocess.Popen(cmdList, stdout=subprocess.PIPE)
+  output = p.stdout
+  result = ''
+  for line in output:
+     if (line != ''):
+        #print line.rstrip('\n')
+        result = result + line
+  return result;
 
-print (UN_SERIAL)
-print (UN_MODEL)
-print (UN_TZ)
-print (UN_RELEASE)
-print (UN_CPUCOUNTS)
-print (UN_PHYCPUS)
-print (PER_CORES)
+
+def cleanup_str(str):
+   if (str != ''):
+      str = str.replace('\n','')
+      str = str.replace('\l','')
+   return str;
+
+def find_uuid(str):
+   if (str != ''):
+
+
+UN_RELEASE_CMD = ["uname", "-r"]
+UN_TZ_CMD = ["cat", "/etc/timezone"]
+UN_MODEL_CMD = ["cat", "/etc/issue"]
+UN_SERIAL_CMD = ["sudo", "/usr/sbin/dmidecode"]
+
+
+result = exec_command(UN_RELEASE_CMD)
+result = cleanup_str(result)
+print result
+
+result = exec_command(UN_TZ_CMD)
+result = cleanup_str(result)
+print result
+
+result = exec_command(UN_MODEL_CMD)
+result = cleanup_str(result)
+print result
+
+result = exec_command(UN_SERIAL_CMD)
+print result
+
+
+#UN_CPUCOUNTS = subprocess.getoutput("cat /proc/cpuinfo | grep processor | wc -l")
+#UN_PHYCPUS = subprocess.getoutput("grep \"physical id\" /proc/cpuinfo | sort -u | wc -l")
+#PER_CORES = subprocess.getoutput("grep \"cores\" /proc/cpuinfo | sort -u | cut -d\':\' -f2")
